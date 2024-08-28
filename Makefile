7348528a10f692c26442180ef51c4f64e55697ae
Peter Reuterås <peter@reuteras.com>
2024-08-28 10:35:42 +0200

    Update

--- Makefile
+++ Makefile
@@ -1,30 +1,30 @@
 #* Variables
 SHELL := /usr/bin/env bash
-PYTHON := python3
+PYTHON := python3.9
 PYTHONPATH := `pwd`
 VENV := .venv
 
 # Virtuell environment
 .PHONY: .venv
 .venv:
-	python3 -m venv "${VENV}"
-	. .venv/bin/activate && python3 -m pip install -U pip
+	python3.9 -m venv "${VENV}"
+	source .venv/bin/activate && python3.9 -m pip install -U pip
 
 #* Installation
 .PHONY: install
 install: .venv
-	. .venv/bin/activate && python3 -m pip install .
+	source .venv/bin/activate && python3.9 -m pip install .
 
 #* Build wheel
 .PHONY: wheel
 wheel: .venv
-	. .venv/bin/activate && python3 -m pip wheel .
+	source .venv/bin/activate && python3.9 -m pip wheel .
 
 #* Uninstall
 #* Installation
 .PHONY: uninstall
 uninstall:
-	. .venv/bin/activate && python3 -m pip uninstall iris_cortexanalyzer_module
+	source .venv/bin/activate && python3.9 -m pip uninstall iris_cortexanalyzer_module
 
 #* Cleanup
 .PHONY: venv-remove
@@ -43,5 +43,9 @@ egg-remove:
 build-remove:
 	rm -rf build/
 
+.PHONY: dist-remove
+dist-remove:
+	rm -rf dist/
+
 .PHONY: clean
-clean: build-remove egg-remove venv-remove wheel-remove
+clean: build-remove dist-remove egg-remove venv-remove wheel-remove
--- buildnpush2iris.sh
+++ buildnpush2iris.sh
@@ -1,50 +1,41 @@
 #!/bin/bash
 
-# Help
-Help()
-{
-   # Display Help
-   echo "This script builds the DFIR-IRIS module of the current directory and installs it to DFIR-IRIS. If you run it for the first time or change something in the module configuration template make sure to run the -a switch."
-   echo
-   echo "Syntax: ./buildnpush2iris [-a|h]"
-   echo "options:"
-   echo "a     Also install the module to the iris-web_app_1 container. Only required on initial install or when changes to config template were made."
-   echo "h     Print this Help."
-   echo
+# Display help
+Help() {
+    # Display Help
+    echo "This script builds the DFIR-IRIS module of the current directory and installs it to DFIR-IRIS. If you run it for the first time or change something in the module configuration template make sure to run the -a switch."
+    echo
+    echo "Syntax: ./buildnpush2iris [-a|h]"
+    echo "options:"
+    echo "a     Also install the module to the iris-web_app_1 container. Only required on initial install or when changes to config template were made."
+    echo "h     Print this Help."
+    echo
 }
 
-Run()
-{
+Run() {
     echo "[BUILDnPUSH2IRIS] Starting the build and push process.."
-    SEARCH_DIR='./dist'
-    get_recent_file () {
-        FILE=$(ls -Art1 ${SEARCH_DIR} | tail -n 1)
-        if [ ! -f "${FILE}" ]; then
-            SEARCH_DIR="${SEARCH_DIR}/${FILE}"
-            get_recent_file
-        fi
-        echo "$FILE"
-        exit
-    }
 
-    python3.9 setup.py bdist_wheel
+    .venv/bin/python3.9 setup.py bdist_wheel > /dev/null
 
-    latest=$(get_recent_file)
-    module=${latest#"./dist/"}
+    latest=$(find dist -type f -name '*.whl' | sort -n | tail -1)
+    module=$(basename "$latest")
 
     echo "[BUILDnPUSH2IRIS] Found latest module file: $latest"
     echo "[BUILDnPUSH2IRIS] Copy module file to worker container.."
-    docker cp "$latest" "iriswebapp_worker:/iriswebapp/dependencies/$module"
+    docker cp "$latest" iriswebapp_worker:/iriswebapp/dependencies/
     echo "[BUILDnPUSH2IRIS] Installing module in worker container.."
-    docker exec -it iriswebapp_worker /bin/sh -c "pip3 install dependencies/$module --force-reinstall"
+
+    docker exec -it iriswebapp_worker /bin/sh -c "python -m pip install -U pip"
+    docker exec -it iriswebapp_worker /bin/sh -c "python -m pip install /iriswebapp/dependencies/$module --force-reinstall"
     echo "[BUILDnPUSH2IRIS] Restarting worker container.."
     docker restart iriswebapp_worker
 
     if [ "$a_Flag" = true ] ; then
         echo "[BUILDnPUSH2IRIS] Copy module file to app container.."
-        docker cp "$latest" "iriswebapp_app:/iriswebapp/dependencies/$module"
+        docker cp "$latest" iriswebapp_app:/iriswebapp/dependencies/
         echo "[BUILDnPUSH2IRIS] Installing module in app container.."
-        docker exec -it iriswebapp_app /bin/sh -c "pip3 install dependencies/$module --force-reinstall"
+        docker exec -it iriswebapp_app /bin/sh -c "python -m pip install -U pip"
+        docker exec -it iriswebapp_app /bin/sh -c "python -m pip install /iriswebapp/dependencies/$module --force-reinstall"
         echo "[BUILDnPUSH2IRIS] Restarting app container.."
         docker restart iriswebapp_app
     fi
@@ -55,20 +46,19 @@ Run()
 a_Flag=false
 
 while getopts ":ha" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-      a) # Enter a name
-         echo "[BUILDnPUSH2IRIS] Pushing to Worker and App container!"
-         a_Flag=true
-         Run
-         exit;;
-     \?) # Invalid option
-         echo "ERROR: Invalid option"
-         exit;;
-
-   esac
+    case $option in
+        h) # Display help message
+            Help
+            exit;;
+        a) # Install module to worker and app container
+             echo "[BUILDnPUSH2IRIS] Pushing to Worker and App container!"
+            a_Flag=true
+            Run
+            exit;;
+        \?) # Invalid option
+            echo "ERROR: Invalid option"
+            exit;;
+    esac
 done
 
 echo "[BUILDnPUSH2IRIS] Pushing to Worker container only!"
